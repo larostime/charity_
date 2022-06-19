b739cbf24a48beb46fbd324de7b643909358b0fc
larostime <mymail166944@gmail.com>
2022-06-19 21:40:43 +0300

    Update constants.php

--- admin/constants.php
+++ admin/constants.php
@@ -3,9 +3,9 @@
 
     define('SITEURL', 'http://localhost/loxury/');
     define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', "loxury");
+    define('DB_USERNAME', 'sfjaxgyxdt');
+    define('DB_PASSWORD', 'QjgC894XGq');
+    define('DB_NAME', "sfjaxgyxdt");
 
     $conn = mysqli_connect (LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
     $db_select = mysqli_select_db ($conn, DB_NAME) or die (mysqli_error());
