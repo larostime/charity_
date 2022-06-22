554eb45afa0c3d2bcb431f660e578e2e217bb4d9
larostime <mymail166944@gmail.com>
2022-06-22 19:01:47 +0300

    Update header.php

--- header.php
+++ header.php
@@ -60,7 +60,7 @@
         <!-- Nav Bar Start -->
         <div class="navbar navbar-expand-lg bg-dark navbar-dark">
             <div class="container-fluid">
-                <a href="index.php" class="navbar-brand">AidUAippt</a>
+                <a href="index.php" class="navbar-brand">AidUA</a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
